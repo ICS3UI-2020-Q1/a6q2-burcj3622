ac25d3ddb20342b84a303f021ce80c4601789779
burcj3622 <burcj3622@wrdsb.ca>
2020-10-08 16:10:58 +0000

    number finder

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -10,6 +11,19 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
+    Scanner input = new Scanner(System.in);
     
+    double [] number = new double [10];
+
+    for(int i = 0; i < number.length;i++){
+    System.out.println("Please enter in 10 integers to put into the array " + (i+1));
+    number[i] = input.nextDouble();
+    }
+    System.out.println("Please enter a number to find");
+    int num = input.nextInt();
+    
+    for (int i = 0; i < number.length; i++){
+    if(num ==number[i]) System.out.println(num  +" is located at array " + i);
+    }
   }
 }
